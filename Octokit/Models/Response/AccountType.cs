1378a432cea3b31b63294e6620bf74f958d7ed5a
AzureAD\AdamMenkes <adamm@bluetread.com>
2017-04-15 16:00:24 -0400

    #15891 Initial checkin of Octokit.NET project minus linqpad files

---
+++ Octokit/Models/Response/AccountType.cs
@@ -0,0 +1,20 @@
+﻿namespace Octokit
+{
+    public enum AccountType
+    {
+        /// <summary>
+        ///  User account
+        /// </summary>
+        User,
+
+        /// <summary>
+        /// Organization account
+        /// </summary>
+        Organization,
+
+        /// <summary>
+        /// Bot account
+        /// </summary>
+        Bot
+    }
+}
